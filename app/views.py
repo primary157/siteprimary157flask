cc1a1a31b7646ae0a2fef24ec27f15b673a48c25
victorgvbh@gmail.com <victorgvbh@gmail.com>
2015-03-10 00:29:59 -0300

    Adiçao da URL de login com o primeiro form do projeto e alguns codigos em javascript. Decisao final da estrutura do codigo da mensagem da pagina inicial. Uso do cmd flash. Criaçao do config.py e modificaçao para host nao local

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from flask import request
 import getpass
+from .forms import LoginForm
+import os
 
 
 @app.route('/')
@@ -11,7 +13,11 @@ def index():
     infos = info.replace(')', ' ').replace('(', ' ').split(' ')
     OS = infos[1] + ' ' + infos[2] + ' ' + infos[3] + ' ' + infos[4]
     OS = OS.replace(';', '')
-    username = getpass.getuser()
+    # username = os.getlogin()
+    # username = request.user.username
+    # username = os.path.split(os.path.expanduser('~'))[-1]
+    # username = getpass.getuser()
+    username = "guest"
     if 'chrome' in info or 'Chrome' in info:
         browser = 'Chrome'
     elif 'Opera' in info or 'opera' in info:
@@ -41,3 +47,16 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 from app import app
-app.run(debug=True,host= '192.168.0.102')
+app.run(debug=True,host= '0.0.0.0')
